bf5fefaaef417cbfd94ffbc277d34bd2412e8c06
man3kin3ko <perevoznikovaks@gmail.com>
2024-05-24 14:50:48 +0300

    Fronted app now serves from Flask
    Added /backend/app/dist soft symlink to /frontend/dist. We can use npm build and then flask run to test new versions.
    
    Don't try to move endpoint logic from __init__.py to app.py. Everything will broke possibly due to the app.app_context().push() expression.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from .db_models import db
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="dist", static_url_path='')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orders.db"
+
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 app.app_context().push()
 db.init_app(app)
