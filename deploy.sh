5699db047bcbbe0126ab737324c6560572578cf3
man3kin3ko <perevoznikovaks@gmail.com>
2024-05-31 16:29:52 +0300

    moved form validation logic to a separate class

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 cd frontend/ && npm install && npm run build
-cd .. && ln -s `pwd`/frontend/dist `pwd`/backend/app/
+cd .. && ln -s `pwd`/frontend/dist `pwd`/backend/app/ || exit 0
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,36 +18,80 @@ function addPic(divId, Img, style=null) {
   }
 }
 
-const emailValidate = (email) => {
-  return String(email)
+class Validator {
+  constructor() {
+    this.validationPassed = true;
+  }
+
+  static emailValidate(contact) {
+    return String(contact)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  static socialNetworksValidate(contact) {
+    return String(contact)
     .toLowerCase()
     .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^([A-Za-z0-9_]{3,25})$/
     );
-};
-
-function socialNetworksValidate(contact) {
-  console.log(contact);
-}
-
-function addValidationWarning(element) {
-  element.classList.add("form-control", "is-invalid");
-
-  let label = document.createElement('div');
-  label.classList.add("invalid-feedback", "text-start");
-  label.innerHTML += "Пожалуйста, проверьте адрес";
-  label.setAttribute("id","emailFeedback");
-
-  contact.parentNode.insertBefore(label, element.nextSibling);
-
-}
-
-function removeValidationWarnig(element) {
-  try {
-    element.classList.remove("is-invalid");
-    element.classList.add("is-valid");
   }
-  catch (e) {};
+  
+  static phoneValidate(contact) {
+    return String(contact)
+    .toLowerCase()
+    .match(
+      /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+    );
+  }
+
+  validate(selectedOption, contact) {
+    const toggleValidationWarning = (elem, callback) => {
+      if (callback(elem.value) == null) {
+        addValidationWarning(elem);
+        return;
+      }
+      removeValidationWarnig(elem);
+    }
+    
+    const addValidationWarning = (element) => {
+      this.validationPassed = false;
+      element.classList.add("form-control", "is-invalid");
+      if (this.label === undefined) {
+        this.label = document.createElement('div');
+        this.label.classList.add("invalid-feedback", "text-start");
+        this.label.innerHTML += "Пожалуйста, проверьте данные";
+      
+        contact.parentNode.insertBefore(this.label, element.nextSibling);
+      }
+    }
+    
+    const removeValidationWarnig = (element) => {
+        this.validationPassed = true;
+        if (!(this.label === undefined)) {
+          element.classList.remove("is-invalid");
+          element.classList.add("is-valid");
+          this.label.remove();
+        }
+    }
+
+    switch (selectedOption) {
+      case "not-selected":
+        alert("Пожалуйста, выберите платформу для свзяи");
+        break;
+      case "email":
+        toggleValidationWarning(contact, this.constructor.emailValidate);
+        break;
+      case "phone":
+        toggleValidationWarning(contact, this.constructor.phoneValidate);
+        break;
+      default:
+        toggleValidationWarning(contact, this.constructor.socialNetworksValidate);
+    }
+    console.log(this)
+  }
 }
 
 if (window.location.pathname == "/") {
@@ -90,47 +134,38 @@ if (window.location.pathname == "/") {
   }
 
 if (window.location.href.includes("repair_order") || window.location.pathname == "/") {
+  let validator = new Validator();
   document.getElementById("formButton").addEventListener('click', async () => {
     let contact = document.getElementById("contact");
     let selectedOption = document.getElementById("socSelect").selectedOptions[0].value;
-    if (selectedOption == "not-selected") {
-      alert("Пожалуйста, выберите платформу для свзяи")
-      return;
+    validator.validate(selectedOption, contact);
+
+    if (validator.validationPassed) {
+      let answer = new FormData(document.getElementById("form"));
+      answer.append("soc_type", selectedOption);
+      await fetch("http://localhost:8000/form", {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Object.fromEntries(answer))
+        })
+        .then(res => {
+          if (res.status == 200) { 
+            alert("Спасибо за обращение");
+            form.reset();
+          }
+          else {
+            console.log(res);
+            alert("Что-то пошло не так");
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Что-то пошло не так. Попробуйте снова");
+        });
+        return false;
     }
-    if (selectedOption == "email") {
-      if (emailValidate(contact.value) == null) {
-        addValidationWarning(contact);
-        return;
-      }
-      else {
-        removeValidationWarnig(contact);
-      }
-    }
-  
-    let answer = new FormData(document.getElementById("form"));
-    answer.append("soc_type", selectedOption);
-    await fetch("http://localhost:8000/form", {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(Object.fromEntries(answer))
-      })
-      .then(res => {
-        if (res.status == 200) { 
-          alert("Спасибо за обращение");
-          form.reset();
-        }
-        else {
-          console.log(res);
-          alert("Что-то пошло не так");
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        alert("Что-то пошло не так. Попробуйте снова");
-      });
-      return false;
   }) 
 }
