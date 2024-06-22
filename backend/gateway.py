9d666b0aa8227bb76537211d0ce44c8935901b28
CHB-0r1s <borisluzin2004@gmail.com>
2024-06-23 01:10:39 +0300

    Add telegram notifications Add set_status handler

--- backend/gateway.py
+++ backend/gateway.py
@@ -1,17 +1,21 @@
 import uuid
 import logging
 from fastapi import FastAPI
-from pydantic import BaseModel
+
 from starlette.middleware.cors import CORSMiddleware
-from app.db_models import db, RepairOrder, SocialMediaType
+from app.db_models import db, RepairOrder
+from app.schemas import OrderFormRequestSchema
+
 from app.telegram_bridge import send_message_via_bot, TG_TOKEN
 from app.dungeon_masters import MastersChatId
-app = FastAPI()
-app.add_middleware(
+from app.messages_builder import build_new_order_message
+
+fast_api = FastAPI()
+fast_api.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["POST", "GET"],
+    allow_methods=["POST", "GET", "PATCH"],
     allow_headers=["*"],
 )
 
@@ -21,14 +25,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class OrderFormRequestSchema(BaseModel):
-    contact: str
-    model: str
-    problem: str
-    soc_type: SocialMediaType
-
-
-@app.get("/catalog")
+@fast_api.get("/catalog")
 def read_root():
     return {
         "Камеры": ["Крутая камера пиздец"],
@@ -52,16 +49,27 @@ def save_data_db(order_form, order_uuid):
     db.session.commit()
 
 
-@app.post("/form")
-def read_item(order_form: OrderFormRequestSchema):
+@fast_api.post("/form")
+async def read_item(order_form: OrderFormRequestSchema):
     try:
-        save_data_db(order_form, str(uuid.uuid4()))
-        send_message_via_bot(TG_TOKEN, MastersChatId.Onjey, "Привет")
+        new_order_uuid = str(uuid.uuid4())
+        # save_data_db(order_form, new_order_uuid)
+
+        cur_master = MastersChatId.Boris
+        new_order_message: str = build_new_order_message(order_form, new_order_uuid, cur_master.name)
+
+        await send_message_via_bot(TG_TOKEN, cur_master.value, new_order_message)
+
         return {}, 200
     except Exception as e:
         return {e.__repr__()}, 500
 
 
-@app.post("/super_sec_root_with_very_long_sec_name")
-def f():
-    pass
+@fast_api.patch("/orders/{order_id}/status/{order_status}")
+async def set_order_status(order_id: str, order_status: str):
+    res = {"order_id": order_id, "order_status": order_status}
+    print(res)
+    return res, 200
+
+
+
