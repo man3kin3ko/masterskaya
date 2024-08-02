9eb0cd8d228eafaf32f168bde06e024118374e1a
kperevoznikova <perevoznikovaks@gmail.com>
2024-08-02 18:39:01 +0300

    added callbackrouter

--- backend/app/telegram_bridge.py
+++ backend/app/telegram_bridge.py
@@ -15,6 +15,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 class Singleton(type):
     _instances = {}
 
@@ -24,29 +25,20 @@ class Singleton(type):
             cls._instances[cls] = instance
         return cls._instances[cls]
 
-class CallbackHelper():
+
+class CallbackRouter:
     def __init__(self):
-        self.types = Status
-        self.last_uuid = None
-        self.last_status = None
+        self.statuses = Status
 
-    def check(self, callback_msg):
-        parts = callback_msg.split(":")
-        assert len(parts) == 2
-        print(len(parts))
-        print(self.types)
-        print(parts[1] in self.types)
-        condition = parts[1] in self.types and True # add uuid check
-        self.last_status = parts[1] if condition else None
-        self.last_uuid = parts[0] if condition else None
-        print(self.last_status)
-        print(self.last_uuid)
-        return condition
+    def update_order(self, callback_msg, master):
+        _, uuid, status = callback_msg.split("/")
+        assert status in self.statuses
+        update_repair_status(uuid, status, master.id)
+        return f"Заказ `{uuid}` принят {master.first_name} {master.last_name if master.last_name else ''}"
 
-    def handle_callback(self, callback_msg, master_id):
-        if (self.check(callback_msg)):
-            update_repair_status(self.last_uuid, self.last_status, master_id)
-
+    def handle_callback(self, callback_msg, master):
+        if (callback_msg.startswith("/order")):
+            return self.update_order(callback_msg, master)
 
 class TelegramBridge(metaclass=Singleton):
     def __init__(self, token, chat):
@@ -54,39 +46,64 @@ class TelegramBridge(metaclass=Singleton):
         self.app.add_handler(CallbackQueryHandler(self.get_button))
         self.app.add_handler(CommandHandler("upload", self.upload_spares))
         self.chat = chat
-        self.callback_helper = CallbackHelper()
+        self.router = CallbackRouter()
 
     def poll(self):
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
     async def send_message(self, message, **kwargs):
-        await self.app.bot.send_message(chat_id=self.chat, text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2, reply_markup=kwargs.get("markup"))
+        await self.app.bot.send_message(
+            chat_id=self.chat,
+            text=message,
+            parse_mode=telegram.constants.ParseMode.MARKDOWN_V2,
+            reply_markup=kwargs.get("markup"),
+        )
 
-    @staticmethod # делается сырым запросом чтобы бот мог получать апдейты в другом процессе
+    @staticmethod  # делается сырым запросом чтобы бот мог получать апдейты в другом процессе
     async def send_new_order(order_form, order_uuid):
         msg_template = "Новый заказ от \[{soc_type}\] {contact}\n\nМодель {model}\n\n```{problem}```\n\nНомер заказа: `{uuid}`"
-        httpx.post(f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage", json={
-            "chat_id":WORKING_CHAT,
-            "text":msg_template.format(
-            uuid=order_uuid, 
-            soc_type=order_form.soc_type.value, 
-            contact=order_form.contact, 
-            model=order_form.model, 
-            problem=order_form.problem
-            ),
-            "parse_mode":telegram.constants.ParseMode.MARKDOWN_V2,
-            "reply_markup":{"inline_keyboard":[[{"text":"Принять","callback_data":f"{order_uuid}:{Status.ACCEPTED.value}"}]]}
-        })
+        httpx.post(
+            f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage",
+            json={
+                "chat_id": WORKING_CHAT,
+                "text": msg_template.format(
+                    uuid=order_uuid,
+                    soc_type=order_form.soc_type.value,
+                    contact=order_form.contact,
+                    model=order_form.model,
+                    problem=order_form.problem,
+                ),
+                "parse_mode": telegram.constants.ParseMode.MARKDOWN_V2,
+                "reply_markup": {
+                    "inline_keyboard": [
+                        [
+                            {
+                                "text": "Принять",
+                                "callback_data": f"/order/{order_uuid}/{Status.ACCEPTED.value}",
+                            }
+                        ]
+                    ]
+                },
+            },
+        )
 
-    async def get_button(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def get_button(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
         query = update.callback_query
         await query.answer()
 
-        self.callback_helper.handle_callback(query.data, query.from_user.id)
-        await query.edit_message_text(text=f"Заказ `{self.callback_helper.last_uuid}` принят {query.from_user.first_name} {query.from_user.last_name if query.from_user.last_name else ''}",parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+        text, kb = self.router.handle_callback(query.data, query.from_user)
+        await query.edit_message_text(
+            text=text,
+            parse_mode=telegram.constants.ParseMode.MARKDOWN_V2,
+        )
 
-    async def upload_spares(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def upload_spares(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
         await update.message.reply_text(context)
+
 
 @click.command("start-bot")
 def start_bot_command():
