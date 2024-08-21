af84ccabaf6f90d8f36b7af4f29d093c10d4a0dc
man3kin3ko <perevoznikovaks@gmail.com>
2024-08-21 15:21:36 +0300

    seo stuff

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,6 @@ class Config:
     ADDRESS = "Санкт-Петербург, Набережная Обводного канала дом 135, офис 11"
     PHONE = "+7 (993) 980 43 30"
     MAIL = ""
-    AVITO_LINK = "https://avito.ru"
+    AVITO_LINK = "https://www.avito.ru/brands/62a50357047c5f3aff4741df28540fb5/"
     VK_LINK = "https://vk.com"
     TG_LINK = "https://t.me/masterskaya35ru"
-    METRO = "Фрунзенская"
